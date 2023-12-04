c235d1abc67da4684414bf3c7b33eee1053cffe9
udebellabre <ubald.debellabre@shodo-studio.io>
2023-12-04 03:55:31 +0100

    compute result for day3

--- src/day3/compute-result.ts
+++ src/day3/compute-result.ts
@@ -1,6 +1,10 @@
-import { countValidNumbers } from "./count-valid-numbers.ts";
-import { readGame } from "./read-game.ts";
+import {countValidNumbers} from "./count-valid-numbers.ts";
+import {readGame} from "./read-game.ts";
+import {countGearsRatio} from "./count-gears-ratio.ts";
 
 const lines = (await Deno.readTextFile("./input.txt")).split("\n");
 const resultRound1 = countValidNumbers(readGame(lines));
-console.log(`[calibrate.ts:11] result round 1`, resultRound1);
+console.log(`[day3] result round 1`, resultRound1);
+
+const resultRound2 = countGearsRatio(readGame(lines));
+console.log(`[day3] result round 2`, resultRound2);
